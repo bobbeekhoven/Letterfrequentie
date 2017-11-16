dd575bea71d70b4a56cd937c857dc8c193b0e47d
bobbeekhoven <bob.vanbeekhoven@itvitaelearning.nl>
2017-11-16 13:51:09 +0100

    V1.0

--- Letterfrequentie/Program.cs
+++ Letterfrequentie/Program.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Letterfrequentie
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // Array to store frequencies
+            int[] c = new int[(int)char.MaxValue];
+
+            //Read entire text file
+            string s = File.ReadAllText("lorem.txt");
+
+            // Iterate over each character
+            foreach (char t in s)
+            {
+                // Increment table
+                c[(int)t]++;
+            }
+            // Write all letters found (press enter to continue)
+            for (int i = 0; i < (int)char.MaxValue; i++)
+            {
+                if (c[i]> 0 && char.IsLetterOrDigit((char)i))
+                {
+                    Console.WriteLine("Letter: {0} Frequency: {1}", (char)i, c[i]);
+                    Console.WriteLine("Press enter for next value");
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
